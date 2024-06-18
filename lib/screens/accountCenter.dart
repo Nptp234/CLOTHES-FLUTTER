5e0cdf685af1148d7d8dd79464d2382d12f74c08
phuoc <103914730+Nptp234@users.noreply.github.com>
2024-06-18 17:08:40 +0700

    update display account center

--- lib/screens/accountCenter.dart
+++ lib/screens/accountCenter.dart
@@ -2,6 +2,7 @@ import 'package:clothes_app/objects/user.dart';
 import 'package:clothes_app/screens/account.dart';
 import 'package:clothes_app/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AccountCenter extends StatefulWidget {
   AccountCenter({super.key});
@@ -16,6 +17,10 @@ class _AccountCenterState extends State<AccountCenter> {
   
   final scaffoldKey = GlobalKey<ScaffoldState>();
   UserAcount user = UserAcount();
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   
   @override
   Widget build(BuildContext context) {
@@ -28,161 +33,92 @@ class _AccountCenterState extends State<AccountCenter> {
           child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0)),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Username",
-                style: TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-            ),
-            ),
-            Container(
-              width: 350,
-                height: 50,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(60))
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 8,
-                      child: TextFormField(
-                        decoration:  InputDecoration(
-                          hintText: user.name,
-                          focusColor: Colors.white,
-                          fillColor: Colors.grey,
-                          border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 0), borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), topLeft: Radius.circular(20),bottomRight: Radius.circular(20),topRight: Radius.circular(20))),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 0), borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), topLeft: Radius.circular(20),bottomRight: Radius.circular(20),topRight: Radius.circular(20))),
-                        ),
-                        onChanged: (value) {
-                          
-                        },
-                      ),
-                    ),
-                    
-                  ],
-                ),
-              ),
-              SizedBox(height: 20,),
-              Text("Email",
-                style: TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-            ),
-            ),
-            Container(
-                height: 50,
-                width: 350,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(60))
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 8,
-                      child: TextFormField(
-                        decoration:  InputDecoration(
-                          hintText: user.email,
-                          focusColor: Colors.white,
-                          fillColor: Colors.grey,
-                          border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 0), borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), topLeft: Radius.circular(20),bottomRight: Radius.circular(20),topRight: Radius.circular(20))),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 0), borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), topLeft: Radius.circular(20),bottomRight: Radius.circular(20),topRight: Radius.circular(20))),
-                        ),
-                        onChanged: (value) {
-                          
-                        },
-                      ),
-                    ),
-                    
-                    
-                  ],
-                ),
-              ),
-              SizedBox(height: 20,),
-              Text("Password",
-                style: TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-            ),
-            ),
-            Container(
-                width: 350,
-                height: 50,
                 
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(60))
-                ),
-                child: Row(
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Expanded(
-                      flex: 8,
-                      child: TextFormField(
-                        obscureText: true,
-                        decoration:  InputDecoration(
-
-                          hintText: user.password,
-                          focusColor: Colors.white,
-                          fillColor: Colors.grey,
-                          border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 0), borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), topLeft: Radius.circular(20),bottomRight: Radius.circular(20),topRight: Radius.circular(20))),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 0), borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), topLeft: Radius.circular(20),bottomRight: Radius.circular(20),topRight: Radius.circular(20))),
-                          
-                        ),
-                        onChanged: (value) {
-                          
-                        },
-                      ),
-                    ),
-                    
+                    _listFormFieldCustom('Username', user.name, usernameController),
+                    _listFormFieldCustom('Email', user.email, emailController),
+                    _listFormFieldCustom('Password', user.password, passwordController),
                   ],
                 ),
-              ),
+                
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(20),
 
-              SizedBox(height:20 ,),
-              ElevatedButton(
-                  onPressed: (){}, 
-                    style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF0060FF), 
-                    padding: const EdgeInsets.all(10.0),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-                  ),
-                  child: const SizedBox(
-                    width: 350,
-                    height: 30,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text('Update Information ', style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.normal),)
-                      ],
+                  child: ElevatedButton(
+                    onPressed: (){}, 
+                      style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF0060FF), 
+                      padding: const EdgeInsets.all(10.0),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                    ),
+                    child: const SizedBox(
+                      width: double.infinity,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Update Information ', style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.normal),)
+                        ],
+                      ),
                     ),
                   ),
                 )
-
-              
               ],
             ),
-            
           ],
         ),
         ),
       ),
     );
   }
+
+  Widget _listFormFieldCustom(String title, String hint, TextEditingController input){
+    return Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(50))
+                  ),
+                  margin: const EdgeInsets.all(20),
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(title, style: TextStyle( fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black,),),
+                      TextFormField(
+                        decoration:  InputDecoration(
+                          hintText: hint,
+                          focusColor: Colors.white,
+                          fillColor: Colors.grey,
+                          border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 0), borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), topLeft: Radius.circular(20),bottomRight: Radius.circular(20),topRight: Radius.circular(20))),
+                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 0), borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), topLeft: Radius.circular(20),bottomRight: Radius.circular(20),topRight: Radius.circular(20))),
+                        ),
+                        controller: input,
+
+                        onChanged: (value) {
+                           
+                        },
+                      ),
+                    ],
+                  ),
+                );
+  }
+
   PreferredSize _headerAccountCenter(){
     return PreferredSize(
       preferredSize: const Size.fromHeight(250),
       child: Stack(
         children: [
           Container(
-            height: 180,
+            height: 190,
             decoration: const BoxDecoration(
               color: Color(0xFF0060FF),
               borderRadius: BorderRadius.only(bottomLeft: Radius.circular(65), bottomRight: Radius.circular(65)),
@@ -200,13 +136,13 @@ class _AccountCenterState extends State<AccountCenter> {
                     
                   children: [
                     IconButton(
-                        onPressed: (){ 
-                          
-                        }, 
-                        icon: const Icon(Icons.arrow_back, size: 30, color: Colors.white,)
-                      ),
+                      onPressed: (){ 
+                        Navigator.pop(context);
+                      }, 
+                      icon: const Icon(Icons.arrow_back, size: 30, color: Colors.white,)
+                    ),
                     const SizedBox(
-                      width: 300,
+                      width: 310,
                       child: Text('Account Center', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20), textAlign: TextAlign.center,),
                     ),
                     
